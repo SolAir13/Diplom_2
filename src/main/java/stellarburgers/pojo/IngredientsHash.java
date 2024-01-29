232846a3a3266c631e97f0b89710a7ccc7ab0c0c
Alexey Solomatin <asolomatin94@yandex.ru>
2024-01-29 19:49:44 +0300

    First commit Diplom_2

---
+++ src/main/java/stellarburgers/pojo/IngredientsHash.java
@@ -0,0 +1,10 @@
+package stellarburgers.pojo;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class IngredientsHash {
+    private final List<String> ingredients;
+}
---
+++ src/main/java/stellarburgers/pojo/RegisterUser.java
@@ -0,0 +1,10 @@
+package stellarburgers.pojo;
+
+import lombok.Data;
+
+@Data
+public class RegisterUser {
+    private final String email;
+    private final String password;
+    private final String name;
+}
